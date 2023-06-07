350f1e9c69a9bef5b5c355a8b2781953198fc248
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-06-07 10:00:35 +0600

    MongoDB successfully connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,8 +12,7 @@ app.use(express.json());
 
 
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.vweq3se.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vweq3se.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
